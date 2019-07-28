aab234a0a959443422a7059119a0d949875daa21
bessfernandez <bess@lumenlearning.com>
2019-07-28 10:59:38 -0700

    Updating demo dir for reference

--- demo/precache-manifest.1376b1383744961abaab05e3d3ee36ba.js
+++ demo/precache-manifest.1376b1383744961abaab05e3d3ee36ba.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5068976f25726b8fa73d97788904e152",
+    "revision": "dc7856b532fcc7a0b3d3263ccf15221e",
     "url": "/gutenberg_search/index.html"
   },
   {
@@ -8,16 +8,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/gutenberg_search/static/css/2.764ccc25.chunk.css"
   },
   {
-    "revision": "e40ef87025dc383cb4ea",
-    "url": "/gutenberg_search/static/css/main.05cc6a6b.chunk.css"
+    "revision": "8c690a5b5368f25a2a2f",
+    "url": "/gutenberg_search/static/css/main.56b9baee.chunk.css"
   },
   {
     "revision": "2738768612297ae81247",
     "url": "/gutenberg_search/static/js/2.fda1c694.chunk.js"
   },
   {
-    "revision": "e40ef87025dc383cb4ea",
-    "url": "/gutenberg_search/static/js/main.04788d03.chunk.js"
+    "revision": "8c690a5b5368f25a2a2f",
+    "url": "/gutenberg_search/static/js/main.6667eb49.chunk.js"
   },
   {
     "revision": "a86209b4976847b04898",
